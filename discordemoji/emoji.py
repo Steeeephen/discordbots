e75376268992b9152206f7eb154aa25f0e3db071
Steeeephen <stephen.ofarrell64@gmail.com>
2022-01-06 01:09:43 +0000

    remove fluff

--- discordemoji/emoji.py
+++ discordemoji/emoji.py
@@ -21,24 +21,16 @@ async def on_message(message):
   if message.author == client.user:
       return
   if message.content.startswith('$emoji'):
-    print(message.attachments[0].url)
     emoji_name = message.content.split(" ")[1]
-    # with open("../../../Downloads/predator.jpg", 'rb') as fd:
     img_data = requests.get(message.attachments[0].url).content
+
     with open('image_name.jpg', 'wb') as handler:
         handler.write(img_data)
     with open("image_name.jpg", "rb") as img:
       img_byte = img.read()
+
       await message.guild.create_custom_emoji(name = emoji_name, image = img_byte)
-    # with open("pic1.png", 'rb') as fd:
-    #   await message.guild.create_custom_emoji(name='my_emoji', image=fd.read())
   else:
-    print("ok")
-    print(message.author)
-    if(message.author.id == 399630387917225984):
-
-      await message.channel.send("hahahahaha")
-    # names[message.content.split(' ')[1]] = message.author.id
-    # print(names)
-
+    return
+  
 client.run(TOKEN)
